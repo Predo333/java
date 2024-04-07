f08d586013e2cb50a3f68b7cc924b20e01860eae
Predo333 <133893193+Predo333@users.noreply.github.com>
2024-04-07 20:54:17 -0300

    Update ex8.js

--- primeirotrimestre/listaprova/ex8.js
+++ primeirotrimestre/listaprova/ex8.js
@@ -1,1 +1,19 @@
+let lado1=(Number (prompt("Digite o lado 1: ")))
+let lado2=(Number (prompt("Digite o lado 2: ")))
+let lado3=(Number (prompt("Digite o lado 3: ")))
 
+
+if (lado1==0 || lado2==0 || lado3==0){
+ window.alert ("Não é um triângulo. ")
+}
+ else{
+ if (lado1==lado2 && lado2==lado3) {
+ window.alert ("O triângulo é equilátero. ")
+ }
+ else if (lado1==lado2 || lado2==lado3 || lado1==lado3){
+ window.alert ("O triângulo é isósceles. ")
+ }
+ else{
+ window.alert ("O triângulo é escaleno. ")
+  }
+   
