6359b95a0a14ae7871412e6e7292b52158769c28
Predo333 <133893193+Predo333@users.noreply.github.com>
2024-04-15 08:26:34 -0300

    Update calculadoraimc.js

--- trabalho/calculadoraimc.js
+++ trabalho/calculadoraimc.js
@@ -4,17 +4,30 @@ let altura = Number(prompt("Digite sua altura"))
 let imc =(peso/(altura*altura))
 
 if (imc <=0 ) {
-console.log("Digite um valor válido")
+window.alert("Digite um valor válido")
 
 }
 else if (imc <=18.5 ) {
-console.log("Abaixo do peso")
+window.alert("Abaixo do peso")
 }
 
 else if (imc >=18.5 && imc <24.9){
-console.log("Peso Normal")
+window.alert("Peso Normal")
 }
 
 else if (imc >=25.0 && imc <29.9){
-console.log("Sobrepeso")
+window.alert("Sobrepeso")
+}
+
+else if (imc >=30.0 && imc <34.9) {
+window.alert("Obesidade Grau I")
+}
+
+else if (imc >=35.0 && imc <39.9) {
+window.alert("Obesidade Grau II (severa)")
+}
+
+else {
+window.alert("Obesidade Grau III (mórbida)")
+
 }
